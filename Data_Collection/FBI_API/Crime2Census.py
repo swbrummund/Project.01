ef3451027232180cc64e4d61ff51a526916c2050
Phiremouse <Byford.Boyd@gmail.com>
2019-09-02 18:08:08 -0500

    all suggested fields except per capita

--- Data_Collection/FBI_API/Crime2Census.py
+++ Data_Collection/FBI_API/Crime2Census.py
@@ -6,7 +6,7 @@ Created on Fri Aug 30 22:34:46 2019
 """
 
 import pandas as pd
-
+# import os
 
 
 def c2c(S_File,C_File):
@@ -22,5 +22,14 @@ def c2c(S_File,C_File):
     Sdf['State']=Sdf['State'].str.upper()
     Sdf['County']=Sdf['County'].str.upper()
     Mdf=Sdf.merge(Cdf, left_on = ['State','County'], right_on = ['State','County'])
+    
+    # Super Amazing fun time of creating MORE columns
+    Mdf['Overal Crime Rate']=Mdf['Total Crime']/Mdf['Population']
+    Mdf['Overal Clear Rate']=Mdf['Total Cleared']/Mdf['Population']
+    Mdf['Workforce 2 Pop']=Mdf['Total Workforce']/Mdf['Population']
+    Mdf['Leo 2 Pop']=Mdf['Total Officers']/Mdf['Population']
+    
+    
+    
 
     Mdf.to_csv('Output/Crime2Census.csv')
---
+++ Data_Collection/FBI_API/VvsNV.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  1 00:38:16 2019
+
+@author: Phiremouse
+"""
+
+
+import pandas as pd
+from pathlib2 import Path
+
+def CleanStepFinal():
+
+    Base_Dir=Path(__file__).resolve().parent
+    myfile=Base_Dir.joinpath('output\FullCrimeStats.csv')
+    mychecker =Base_Dir.joinpath('Resources\CrimeListing.csv')
+    df= pd.read_csv(myfile)
+    df2 = pd.read_csv(mychecker)
+    df2.insert(1,column='tmp',value='Crime Cleared Count ' + df2['Crime'])
+    df2.insert(2,column='tmp2',value='Total Crime Count ' + df2['Crime'])
+    
+    
+    df2v=df2[df2['Type']=='Violent']
+    df2n=df2[df2['Type']=='Non-Violent']
+    
+    Cleared = df.filter(regex='Cleared ')
+    Crime = df.filter(regex='Crime Count ')
+    
+    Cleared['Total Violent Cleared']=Cleared[df2v['tmp']].sum(axis =1 )
+    Cleared['Total Non-Violent Cleared']=Cleared[df2n['tmp']].sum(axis =1 )
+    
+    Crime['Total Violent Crime']=Crime[df2v['tmp2']].sum(axis =1 )
+    Crime['Total Non-Violent Crime']=Crime[df2n['tmp2']].sum(axis =1 )
+    
+    Cleared=Cleared.filter(['Total Violent Cleared','Total Non-Violent Cleared'])
+    Crime=Crime.filter(['Total Violent Crime','Total Non-Violent Crime'])
+    Merge1 = pd.concat([Cleared, Crime],join='inner', axis=1, sort=False)
+    
+    Result=pd.concat([df,Merge1],axis=1,join='inner', sort=False)
+    Result.to_csv('output/FullCrimeStatsFinal.csv')
