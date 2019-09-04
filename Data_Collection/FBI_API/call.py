11e13e0f2537b48d2bfbde7947e209b0629b8b97
Phiremouse <Byford.Boyd@gmail.com>
2019-09-04 14:37:10 -0500

    cleaned up the comments in call.py

--- Data_Collection/FBI_API/call.py
+++ Data_Collection/FBI_API/call.py
@@ -2,9 +2,11 @@
 """
 Created on Wed Aug 28 21:11:35 2019
 Main Run
+
 @author: Phiremouse
 """
 
+# Module Section
 import os
 from pathlib2 import Path
 from ORI_Translate import ORI_Transform
@@ -13,30 +15,31 @@ from GROUP_V2_Employment import Agg_Employment
 from GROUP_V2_cleaner import clean_cr_e
 from VvsNV import CleanStepFinal
 from Crime2Census import c2c
+
+# Declared Vairbles
 sampleFile = os.path.join('..','Census_API','sample.csv')
 oriFile = os.path.join('.','Resources','ORI_Dataset.csv')
-#FBISampleFile = os.path.join('output','FBI_Sample_List.csv')
 Base_Dir=Path(__file__).resolve().parent
 FBISampleFile=Base_Dir.joinpath('output\FBI_Sample_List.csv')
-# logic to desconstruct and transform sample from Census to line up to the FBI API
-#ORI_Transform(sampleFile,oriFile,FBISampleFile)
-
-# # 'Logic to create the crime rate csv'
-#SumCrime(FBISampleFile,2016)
-
-# # 'Logic to create the  Police Agency Employement (PAE) csv'
-#Agg_Employment(FBISampleFile,2016)
-
-# CLEAN UP CRIME RATE AND EMPLOYMNENT FILES
-# CrimeFile = os.path.join('.','output','cr_breakdown.csv')
-# EmploymentFile = os.path.join('.','output','E_Breakdown.csv')
-# clean_cr_e(CrimeFile,EmploymentFile,'County')
+C_File =Base_Dir.joinpath('output\FullCrimeStatsFinal.csv')
+CrimeFile = os.path.join('.','output','cr_breakdown.csv')
+EmploymentFile = os.path.join('.','output','E_Breakdown.csv')
 
 
+# Pulls in census sample, reduces it to needed columns, creates bridge file b/w census & crime data.
+ORI_Transform(sampleFile,oriFile,FBISampleFile)
 
-# 'Bring it back to the central dataframe from census api'
-#fbi table
+# based on the bridge file requests are made to pull in crime and cleared stats for a given agency
+SumCrime(FBISampleFile,2016)
+
+# based on the bridge file requests are made to pull in employment information for a given agency
+Agg_Employment(FBISampleFile,2016)
+
+# groups agency employment and crime states data and merge them. Renames columns and creates calculated columns.
+clean_cr_e(CrimeFile,EmploymentFile,'County')
+
+# creates total columns on the crime and cleared columns based on nonviolent and violent offenses
 CleanStepFinal()
 
-C_File =Base_Dir.joinpath('output\FullCrimeStatsFinal.csv')
+# merges the FBI API data to the census sample.
 c2c(sampleFile,C_File)
